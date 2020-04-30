3cedfad1069504da4f9ec2c22e8326df82e55408
Trym Lekva <Trym.Tarjeison.Lekva@dnb.no>
2020-04-30 08:36:56 +0200

    added script to install aws amazon linux 2 dependencies when creating a new instance

---
+++ scripts/aws/install_amazon_linux_packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Yum packages for Amazon Linux 2
+sudo yum install upgrade
+sudo yum install python3
+sudo yum install python3-devel
+sudo yum install gcc
+sudo yum install gcc-c++
